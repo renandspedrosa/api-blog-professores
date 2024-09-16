3f642f705081b1c23422ee6ce2bedf8bc382e392
thiagotnaraujo <thago@crowdability.com>
2024-09-16 09:44:52 -0300

    Controller post delete

--- src/http/controllers/post/delete-post.ts
+++ src/http/controllers/post/delete-post.ts
@@ -2,9 +2,11 @@ import { Request, Response } from 'express'
 import { makeDeletePostUseCase } from '@/use-cases/factory/post/make-delete-post-use-case'
 
 export async function deletePost(req: Request, res: Response) {
-  const deletePostUseCase = makeDeletePostUseCase()
   const { id } = req.params
-  const deletedPost = await deletePostUseCase.handler(id)
 
-  return res.status(201).json(deletedPost)
+  const deletePostUseCase = makeDeletePostUseCase()
+
+  await deletePostUseCase.handler(id)
+
+  return res.status(204).send()
 }
