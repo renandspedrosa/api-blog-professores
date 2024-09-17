aed4b02654f660f690f23b99baa34a235249542c
thiagotnaraujo <thago@crowdability.com>
2024-09-16 21:00:01 -0300

    Método delete para postagem funcionando

--- src/http/controllers/post/routes.ts
+++ src/http/controllers/post/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { create } from '@/http/controllers/post/create'
 import { validateCreatePost } from '@/http/middlewares/post/validation-create-post'
+import { validationFindPost } from '@/http/middlewares/post/validation-find-post'
 import { findPostByTeacher } from '@/http/controllers/post/find-post-by-teacher'
 import { deletePost } from '@/http/controllers/post/delete-post'
 
@@ -8,6 +9,6 @@ const router = Router()
 
 router.post('/', validateCreatePost, create)
 router.get('/teacher/:teacherId', findPostByTeacher)
-router.delete('/post/:id', deletePost)
+router.delete('/:id', validationFindPost, deletePost)
 
 export default router
