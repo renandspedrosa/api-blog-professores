e0217987f7a3e1433a7b098d2055e9fcf25edbc3
Renan Pedrosa <renandspedrosa@gmail.com>
2024-10-03 19:31:11 -0300

    criando try cath

--- src/http/controllers/post/find-post-by-teacher.ts
+++ src/http/controllers/post/find-post-by-teacher.ts
@@ -1,18 +1,26 @@
 import { makeFindPostByTeacherUseCase } from '@/use-cases/factory/post/make-find-post-by-teacher-use-case'
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { z } from 'zod'
 
-export async function findPostByTeacher(req: Request, res: Response) {
-  const registerParamsSchema = z.object({
-    teacherId: z.coerce.number(),
-  })
+export async function findPostByTeacher(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const registerParamsSchema = z.object({
+      teacherId: z.coerce.number(),
+    })
 
-  const { teacherId } = registerParamsSchema.parse(req.params)
-  const { page, limit } = req.body
+    const { teacherId } = registerParamsSchema.parse(req.params)
+    const { page, limit } = req.body
 
-  const findPostByTeacherUseCase = makeFindPostByTeacherUseCase()
+    const findPostByTeacherUseCase = makeFindPostByTeacherUseCase()
 
-  const post = await findPostByTeacherUseCase.handler(teacherId, page, limit)
+    const post = await findPostByTeacherUseCase.handler(teacherId, page, limit)
 
-  return res.status(200).send(post)
+    return res.status(200).send(post)
+  } catch (error) {
+    next(error)
+  }
 }
--- src/http/controllers/post/find-post-by-term.ts
+++ src/http/controllers/post/find-post-by-term.ts
@@ -1,12 +1,20 @@
 import { makeFindPostByTermUseCase } from '@/use-cases/factory/post/make-find-post-by-term-use-case'
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 
-export async function findPostByTerm(req: Request, res: Response) {
-  const { term, page, limit } = req.body
+export async function findPostByTerm(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { term, page, limit } = req.body
 
-  const findPostByTermUseCase = makeFindPostByTermUseCase()
+    const findPostByTermUseCase = makeFindPostByTermUseCase()
 
-  const post = await findPostByTermUseCase.handler(term, page, limit)
+    const post = await findPostByTermUseCase.handler(term, page, limit)
 
-  return res.status(200).send(post)
+    return res.status(200).send(post)
+  } catch (error) {
+    next(error)
+  }
 }
