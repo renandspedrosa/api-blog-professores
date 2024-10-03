1208231aa01a4ba08c4b7c714a0480b892f0cae8
Renan Pedrosa <renandspedrosa@gmail.com>
2024-10-03 16:13:41 -0300

    teste tag

---
+++ src/tests/entities/tag.entity.test.ts
@@ -0,0 +1,42 @@
+import { Tag } from '@/entities/tag.entity'
+import { Post } from '@/entities/post.entity'
+
+describe('Entity Tag', () => {
+  it('It should create a new tag object and define its values', async () => {
+    const newTag = new Tag()
+    newTag.id = 1
+    newTag.name = 'Test Tag'
+    newTag.created_at = new Date()
+    newTag.updated_at = new Date()
+    newTag.status = true
+
+    expect(newTag).toBeInstanceOf(Tag)
+    expect(newTag.id).toBe(1)
+    expect(newTag.name).toBe('Test Tag')
+    expect(newTag.created_at).toBeInstanceOf(Date)
+    expect(newTag.updated_at).toBeInstanceOf(Date)
+    expect(newTag.status).toBe(true)
+    expect(newTag.posts).toBeUndefined()
+  })
+
+  it('It should associate posts with the tag', async () => {
+    const newTag = new Tag()
+    newTag.id = 2
+    newTag.name = 'Another Tag'
+    newTag.status = false
+
+    const post1 = new Post()
+    post1.id = '4e106c23-98cd-456a-955d-adc35295d5d4'
+    post1.title = 'Post 1'
+
+    const post2 = new Post()
+    post2.id = '591d008b-95e1-47a0-a1e8-33a84b8a02f2'
+    post2.title = 'Post 2'
+
+    newTag.posts = [post1, post2]
+
+    expect(newTag.posts).toHaveLength(2)
+    expect(newTag.posts).toContainEqual(post1)
+    expect(newTag.posts).toContainEqual(post2)
+  })
+})
