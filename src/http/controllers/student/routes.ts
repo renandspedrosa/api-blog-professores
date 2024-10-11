30fcda2a41d5668b0b972232b7d3544aef30e1d5
pedrovbo <pedrovicboliveira@gmail.com>
2024-10-11 01:08:16 -0300

    feat:retornar name e email na criação de Student #125

--- src/http/controllers/student/routes.ts
+++ src/http/controllers/student/routes.ts
@@ -3,14 +3,18 @@ import { getAllStudents } from '@/http/controllers/student/get-all-students'
 import { create } from '@/http/controllers/student/create'
 import { deleteStudent } from './delete'
 import { validationFindAll } from '@/http/middlewares/utils/validation-find-all'
-// import { isNotStudent } from '@/http/middlewares/student/is-not-student'
-import { validateCreateUser } from '@/http/middlewares/user/validation-create-user'
 import { validateCreateUserWithUniqueEmail } from '@/http/middlewares/user/validation-unique-email'
+import { validateCreateStudent } from '@/http/middlewares/student/validation-create-student'
 
 const router = Router()
 
 router.get('/', validationFindAll, getAllStudents)
-router.post('/', validateCreateUserWithUniqueEmail, validateCreateUser, create)
+router.post(
+  '/',
+  validateCreateUserWithUniqueEmail,
+  validateCreateStudent,
+  create,
+)
 router.delete('/:id', deleteStudent)
 
 export default router
