4c3a928166b2da27ce5852b8828ba496c032ef6c
Renan Pedrosa <renandspedrosa@gmail.com>
2024-10-11 10:24:22 -0300

    removendo rota create user

--- src/http/controllers/user/routes.ts
+++ src/http/controllers/user/routes.ts
@@ -1,16 +1,11 @@
 import { Router } from 'express'
-import { create } from './create'
 import { findUser } from './find-user'
-import { validateCreateUser } from '@/http/middlewares/user/validation-create-user'
 import { signin } from './signin'
-import { validateCreateUserWithUniqueEmail } from '@/http/middlewares/user/validation-unique-email'
 import { update } from './update'
 import { validateLoginUser } from '@/http/middlewares/user/validation-login-user'
-// import { validateJwt } from '@/http/middlewares/jwt-validate'
 
 const router = Router()
 
-router.post('/', validateCreateUser, validateCreateUserWithUniqueEmail, create)
 router.get('/:id', findUser)
 router.put('/:id', update)
 router.post('/signin', validateLoginUser, signin)
