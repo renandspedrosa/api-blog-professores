53e54e3473c96a058547ddd0509771f155338a5e
Renan Pedrosa <renandspedrosa@gmail.com>
2024-10-13 08:48:12 -0300

    doc student

--- src/http/controllers/student/routes.ts
+++ src/http/controllers/student/routes.ts
@@ -8,13 +8,97 @@ import { validateCreateStudent } from '@/http/middlewares/student/validation-cre
 
 const router = Router()
 
-router.get('/', validationFindAll, getAllStudents)
+/**
+ * @swagger
+ * /student:
+ *   post:
+ *     summary: Cria um novo estudante
+ *     tags:
+ *       - Student
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Nome do estudante
+ *                 example: "João Silva"
+ *               email:
+ *                 type: string
+ *                 description: Email do estudante
+ *                 example: "joao.silva@example.com"
+ *               password:
+ *                 type: string
+ *                 description: Senha do estudante
+ *                 example: "senhaSegura123"
+ *     responses:
+ *       201:
+ *         description: Estudante criado com sucesso
+ */
+
 router.post(
   '/',
   validateCreateUserWithUniqueEmail,
   validateCreateStudent,
   create,
 )
+
+/**
+ * @swagger
+ * /student:
+ *   get:
+ *     summary: Recupera todos os estudantes
+ *     tags:
+ *       - Student
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           description: Número da página para paginar os resultados
+ *           example: 1
+ *       - in: query
+ *         name: limit
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           description: Limite de estudantes a serem retornados por página
+ *           example: 10
+ *     responses:
+ *       200:
+ *         description: Lista de estudantes recuperada com sucesso
+ */
+
+router.get('/', validationFindAll, getAllStudents)
+
+/**
+ * @swagger
+ * /student/{id}:
+ *   delete:
+ *     summary: Deleta um estudante pelo ID
+ *     tags:
+ *       - Student
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do estudante a ser deletado
+ *         example: 1
+ *     responses:
+ *       204:
+ *         description: Estudante deletado com sucesso
+ */
+
 router.delete('/:id', deleteStudent)
 
 export default router
