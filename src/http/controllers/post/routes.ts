d024c1c70e2733d1d5b4e6b2ad4a13367e60beb8
Renan Pedrosa <renandspedrosa@gmail.com>
2024-10-13 10:36:14 -0300

    doc rota posts

--- src/http/controllers/post/routes.ts
+++ src/http/controllers/post/routes.ts
@@ -19,14 +19,304 @@ import { isStudent } from '@/http/middlewares/student/is-student'
 
 const router = Router()
 
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Cria um novo post
+ *     tags:
+ *       - Post
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - content
+ *               - teacher_id
+ *               - tags
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: Título do post
+ *                 example: "Meu Primeiro Post"
+ *               content:
+ *                 type: string
+ *                 description: Conteúdo do post
+ *                 example: "Este é o conteúdo do meu primeiro post."
+ *               teacher_id:
+ *                 type: integer
+ *                 description: ID do professor associado ao post
+ *                 example: 1
+ *               tags:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 description: Lista de tags associadas ao post
+ *                 example: [{"name":"educação"}]
+ *     responses:
+ *       201:
+ *         description: Post criado com sucesso
+ */
+
 router.post('/', isTeacher, validateCreatePost, create)
-router.get('/teacher/:teacherId', validationFindAll, findPostByTeacher)
+
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Recupera todos os posts
+ *     tags:
+ *       - Post
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           example: 1
+ *         description: Número da página para paginação dos resultados
+ *       - in: query
+ *         name: limit
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           example: 15
+ *         description: Número máximo de posts a serem retornados por página
+ *       - in: query
+ *         name: tag
+ *         required: false
+ *         schema:
+ *           type: string
+ *         description: ID da Tag para filtrar os posts retornados
+ *     responses:
+ *       200:
+ *         description: Lista de posts recuperada com sucesso
+ */
+
 router.get('/', validationFindAllPost, findAllPost)
+
+/**
+ * @swagger
+ * /posts/teacher/{teacherId}:
+ *   get:
+ *     summary: Recupera todos os posts de um professor específico
+ *     tags:
+ *       - Post
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: teacherId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *           example: 1
+ *         description: ID do professor para filtrar os posts
+ *       - in: query
+ *         name: page
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           example: 1
+ *         description: Número da página para paginação dos resultados
+ *       - in: query
+ *         name: limit
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           example: 10
+ *         description: Número máximo de posts a serem retornados por página
+ *     responses:
+ *       200:
+ *         description: Lista de posts do professor recuperada com sucesso
+ */
+
+router.get('/teacher/:teacherId', validationFindAll, findPostByTeacher)
+
+/**
+ * @swagger
+ * /posts/search:
+ *   get:
+ *     summary: Busca posts pelo termo especificado
+ *     tags:
+ *       - Post
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: term
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "Meu Primeiro"
+ *         description: Termo a ser pesquisado nos posts
+ *       - in: query
+ *         name: page
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           example: 1
+ *         description: Número da página para paginação dos resultados
+ *       - in: query
+ *         name: limit
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           example: 10
+ *         description: Número máximo de posts a serem retornados por página
+ *     responses:
+ *       200:
+ *         description: Lista de posts que correspondem ao termo de pesquisa
+ */
+
 router.get('/search', validationFindByTerm, findPostByTerm)
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   put:
+ *     summary: Atualiza um post existente
+ *     tags:
+ *       - Post
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "c3c882b2-070e-43d2-81ac-acba3e888e31"
+ *         description: ID do post a ser atualizado
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 example: "Título do Post Atualizado"
+ *                 description: Novo título do post
+ *               content:
+ *                 type: string
+ *                 example: "Conteúdo do post atualizado."
+ *                 description: Novo conteúdo do post
+ *               tags:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 example: []
+ *                 description: Lista de tags associadas ao post
+ *     responses:
+ *       200:
+ *         description: Post atualizado com sucesso
+ */
+
 router.put('/:id', isTeacher, validationFindPost, updatePost)
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Busca um post específico
+ *     tags:
+ *       - Post
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "75e47797-09ce-46c0-b8bb-d8e0d7555dc1"
+ *         description: ID do post a ser buscado
+ *     responses:
+ *       200:
+ *         description: Post encontrado
+ */
+
 router.get('/:id', validationFindPost, findPost)
+
+/**
+ * @swagger
+ * /posts/{post_id}/comments:
+ *   get:
+ *     summary: Busca os comentários de um post específico
+ *     tags:
+ *       - Post
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: post_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "75e47797-09ce-46c0-b8bb-d8e0d7555dc1"
+ *         description: ID do post para o qual os comentários serão buscados
+ *     responses:
+ *       200:
+ *         description: Comentários encontrados
+ */
+
 router.get('/:id/comments', findCommentsByPostId)
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   delete:
+ *     summary: Remove um post específico
+ *     tags:
+ *       - Post
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "75e47797-09ce-46c0-b8bb-d8e0d7555dc1"
+ *         description: ID do post que será removido
+ *     responses:
+ *       204:
+ *         description: Post removido com sucesso
+ */
+
 router.delete('/:id', isTeacher, validationFindPost, deletePost)
+
+/**
+ * @swagger
+ * /posts/{post_id}/viewed:
+ *   post:
+ *     summary: Registra a visualização de um post por um estudante, PRECISA ESTAR LOGADO COMO ESTUDANTE
+ *     tags:
+ *       - Post
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: post_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "75e47797-09ce-46c0-b8bb-d8e0d7555dc1"
+ *         description: ID do post que foi visualizado
+ *     responses:
+ *       201:
+ *         description: Visualização do post registrada com sucesso
+ */
+
 router.post(
   '/:post_id/viewed',
   isStudent,
