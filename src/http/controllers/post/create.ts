cffc1646ab9fae09b51a31aee5f6bd497587c127
gabriel <glima@iibrasil.com.br>
2024-12-04 20:53:35 -0300

    teste

--- src/http/controllers/post/create.ts
+++ src/http/controllers/post/create.ts
@@ -3,30 +3,28 @@ import { makeCreatePostUseCase } from '@/use-cases/factory/post/make-create-post
 import path from 'path';
 import multer from 'multer';
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads/');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   }
+// });
 
-const upload = multer({ storage });
+
 export async function create(req: Request, res: Response, next: NextFunction) {
+  // console.log(req); return false;
   try {
-
+    const upload = multer({ dest:'@/uploads/' });
     upload.single('attachment')(req, res, async (err) => {
-      if (err) {
-        return next(err);
-      }
-
+     console.log(req)
       const createPostUseCase = makeCreatePostUseCase();
       const { title, content, teacher_id, tags } = req.body;
 
       const filePath = req.file ? req.file.path : null;
-      console.log('req.file:', req.file); // Arquivo enviado
-      console.log('req.body:', req.body); // Outros campos
+      console.log('req.file:', req.file);
+      console.log('req.body:', req.body);
 
       const createdPost = await createPostUseCase.handler({
         title,
