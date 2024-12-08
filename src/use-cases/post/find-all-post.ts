d4d7a48c02d07e9d9860258ec8868b51b6b6583b
Renan Pedrosa <renandspedrosa@gmail.com>
2024-12-08 12:34:09 -0300

    filtro tags

--- src/use-cases/post/find-all-post.ts
+++ src/use-cases/post/find-all-post.ts
@@ -3,8 +3,13 @@ import { IPostRepository } from '@/repositories/post.repository.interface'
 export class FindAllPostUseCase {
   constructor(private postRepository: IPostRepository) {}
 
-  async handler(page: number, limit: number, tagId?: number, term?: string) {
-    const posts = await this.postRepository.findAll(page, limit, tagId, term)
+  async handler(
+    page: number,
+    limit: number,
+    tagIds?: Array<number>,
+    term?: string,
+  ) {
+    const posts = await this.postRepository.findAll(page, limit, tagIds, term)
     if (!posts) {
       throw new Error('Postagem não encontrada')
     }
