6ba7edd73bfe0e4bfaf31658713d6cbe9a1a7386
gabriel <glima@iibrasil.com.br>
2024-12-14 23:33:17 -0300

    back end

--- src/http/controllers/post/update-post.ts
+++ src/http/controllers/post/update-post.ts
@@ -1,27 +1,31 @@
-import { Request, Response, NextFunction } from 'express'
-import { makeUpdatePostUseCase } from '@/use-cases/factory/post/make-update-post-use-case'
+import { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import { makeUpdatePostUseCase } from '@/use-cases/factory/post/make-update-post-use-case';
 
 export async function updatePost(
-  req: Request,
-  res: Response,
-  next: NextFunction,
+    req: Request,
+    res: Response,
+    next: NextFunction,
 ) {
   try {
-    const { id } = req.params
-    const { title, content, path_img, tags } = req.body
+    const { id } = req.params;
+    const { title, content, tags } = req.body;
+    const filePath = req.file ? req.file.path : null;
 
-    const updatePostUseCase = await makeUpdatePostUseCase()
+    console.log('Arquivo salvo em:', filePath);
 
-    const post = await updatePostUseCase.handler({
+    const updatePostUseCase = await makeUpdatePostUseCase();
+
+    const updatedPost = await updatePostUseCase.handler({
       id,
       title,
       content,
-      path_img,
+      path_img: filePath ? path.relative(process.cwd(), filePath) : req.body.path_img,
       tags,
-    })
+    });
 
-    return res.status(200).json(post)
+    return res.status(200).json(updatedPost);
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
