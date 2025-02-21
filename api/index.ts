d39942dd902f922dc006fe6d832286a85354ca8f
Renan Pedrosa <renandspedrosa@gmail.com>
2025-02-20 22:19:13 -0300

    rodando

---
+++ api/index.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+import app from '../dist/app'
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return app(req, res)
+}
